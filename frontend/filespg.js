a717e7607ffce349bdb805978ece0ebf71e3ece3
Seb <efseby@gmail.com>
2026-07-09 15:06:42 +0300

    finished search and filter js implementation for filespg

--- frontend/filespg.js
+++ frontend/filespg.js
@@ -1,15 +1,76 @@
-const search = document.querySelector('#search-form');
+const search = document.querySelector ('#search-form');
+const filter = document.querySelector ('#filter');
+const profileCardsContainer = document.querySelector ('#profile-cards');
+const profileCards = document.querySelectorAll ('#profile-cards a');
 
+const cardsArray = Array.from(profileCards);
+
+// live search:
 search.addEventListener('input', function(event){
-    const currentText = event.target.value;
+    const currentText = event.target.value.toLowerCase();
 
-    console.log("user is searching for " + currentText);
+    profileCards.forEach(function(card){
+        
+        const cardText = card.textContent.toLowerCase();
+
+        if (cardText.includes(currentText)){
+            card.style.display = '';
+        }
+        else {
+            card.style.display = 'none';
+        }
+    });
 });
 
-const profileCards = document.querySelectorAll('#profile-cards a');
+//dropdown filter:
+filter.addEventListener('change' , function(event){
+    const option = event.target.value;
 
-profileCards.forEach(function(card){
-    console.log(card.textContent);
+    cardsArray.sort(function(CardA, CardB){
+        const textA = CardA.textContent.toLowerCase().trim();
+        const textB = CardB.textContent.toLowerCase().trim();
+
+        if(option === "alphabetically"){
+            return textA.localeCompare(textB);
+        }
+        else if(option === "reverse-alphabetically"){
+            return textB.localeCompare(textA);
+        }
+    });
+
+    cardsArray.forEach(function(card) {
+        profileCardsContainer.appendChild(card);
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
