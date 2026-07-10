861a0c3232f4689053f191ddc993082af4b1dd03
Seb <efseby@gmail.com>
2026-07-10 11:29:41 +0300

    made profile files dynamic and re-wrote the search function

--- frontend/filespg.js
+++ frontend/filespg.js
@@ -1,25 +1,54 @@
-const search = document.querySelector ('#search-form');
-const filter = document.querySelector ('#filter');
-const profileCardsContainer = document.querySelector ('#profile-cards');
+
 const profileCards = document.querySelectorAll ('#profile-cards a');
+const filter = document.querySelector ('#filter');
+
 
 const cardsArray = Array.from(profileCards);
 
+//profile cards:
+const profileCardsContainer = document.querySelector ('#profile-cards');
+
+let profiles = [
+    {lastname: "Aris", firstname: "Giani"},
+    {lastname: "Dumitru", firstname: "Ion"},
+    {lastname: "Tepes", firstname: "Vlad"},
+];
+
+function renderCards(dataArray) {
+    profileCardsContainer.innerHTML = '';
+
+    dataArray.forEach(function(profile){
+    const htmlString = 
+            `<a href="profile.html" class="flex flex-col items-center justify-center p-4 rounded-xl cursor-pointer outline-none bg-olive-950 hover:bg-zinc-900">
+                    <svg class="w-32 h-32 text-zinc-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" focusable="false" aria-hidden="true">
+                        <path d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z"/>
+                    </svg>
+                    <div class="mt-3 text-white font-mono font-bold text-sm text-center leading-tight">
+                        <p>${profile.lastname}</p>
+                        <p class="text-zinc-400">${profile.firstname}</p>
+                    </div>
+            </a>`;
+
+        profileCardsContainer.insertAdjacentHTML('beforeend', htmlString);
+});
+}
+
+renderCards(profiles);
+
+
+
 // live search:
+const search = document.querySelector ('#search-form');
+
 search.addEventListener('input', function(event){
     const currentText = event.target.value.toLowerCase();
 
-    profileCards.forEach(function(card){
-        
-        const cardText = card.textContent.toLowerCase();
-
-        if (cardText.includes(currentText)){
-            card.style.display = '';
-        }
-        else {
-            card.style.display = 'none';
-        }
+    const filteredProfiles = profiles.filter(function(profile){
+        const fullName = profile.lastname.toLocaleLowerCase() + " " + profile.firstname.toLocaleLowerCase();
+        return fullName.includes(currentText);
     });
+
+    renderCards(filteredProfiles);
 });
 
 //dropdown filter:
@@ -42,35 +71,4 @@ filter.addEventListener('change' , function(event){
         profileCardsContainer.appendChild(card);
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
