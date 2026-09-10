989448415679c4d6430a6d83607f54a3c0a0710d
Seby0769 <efseby@gmail.com>
2026-09-10 13:08:19 +0300

    added Spring Data JPA

--- backend/src/main/java/org/example/backend/Profile.java
+++ backend/src/main/java/org/example/backend/Profile.java
@@ -1,8 +1,18 @@
 package org.example.backend;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 import java.time.LocalDate;
 
+@Entity
 public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     String lastName;
     String firstName;
     LocalDate dateOfBirth;
