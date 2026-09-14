661667bd60074ab9fc061ca4a1d805628d1b4f24
Seby0769 <efseby@gmail.com>
2026-09-14 14:05:38 +0300

    wrote a one time post with CommandLineRunner to check database persistent state + an actuall post endpoint

--- backend/src/main/java/org/example/backend/controller/ProfileController.java
+++ backend/src/main/java/org/example/backend/controller/ProfileController.java
@@ -3,6 +3,8 @@ package org.example.backend.controller;
 import org.example.backend.model.Profile;
 import org.example.backend.repository.ProfileRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -16,8 +18,15 @@ public class ProfileController {
     public ProfileController(ProfileRepository profileRepository){
         this.profileRepository = profileRepository;
     }
+
     @GetMapping("/profiles")
     public List<Profile> getProfiles(){
         return profileRepository.findAll();
+    }
+
+    @PostMapping("/profiles")
+    public Profile createProfile(@RequestBody Profile profile){
+        Profile newProfile = profileRepository.save(profile);
+        return newProfile;
     }
 }
