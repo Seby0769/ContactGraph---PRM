eec594122f3698733c57362ceb35c71353d13b68
Seby0769 <efseby@gmail.com>
2026-09-24 10:17:38 +0300

    Added Login Endpoint

--- backend/src/main/java/org/example/backend/controller/UserController.java
+++ backend/src/main/java/org/example/backend/controller/UserController.java
@@ -1,15 +1,21 @@
 package org.example.backend.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.example.backend.dto.UserRequest_DTO;
 import org.example.backend.model.User;
 import org.example.backend.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @RestController
@@ -24,8 +30,8 @@ public class UserController {
 
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody UserRequest_DTO dto){
-        Optional<User> userOptional = userRepository.findByUsername(dto.getUsername());
-        if (userOptional.isPresent()){
+        Optional<User> userToCheck = userRepository.findByUsername(dto.getUsername());
+        if (userToCheck.isPresent()){
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
         else {
@@ -35,4 +41,26 @@ public class UserController {
         }
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody UserRequest_DTO dto, HttpServletRequest request){
+        Optional<User> userToCheck = userRepository.findByUsername(dto.getUsername());
+        if (userToCheck.isPresent()){
+            User user = userToCheck.get();
+            if (passwordEncoder.matches(dto.getPassword(), user.getPassword())){
+                HttpSession session = request.getSession();
+                session.setAttribute("UserId", user.getId());
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(user.getUsername(), null, new ArrayList<>());
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+                session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+                return ResponseEntity.status(HttpStatus.OK).build();
+            }
+            else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
 }
